aeceaf84699faaa548b1c400dcfc095a47640bc7
kenath\kenath perera <kenathperera222@gmail.com>
2021-04-07 17:49:34 +1000

    Solved Naming Convention in Json Result

--- Startup.cs
+++ Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Newtonsoft.Json.Serialization;
 using WebAPI.Models;
 
 namespace WebAPI
@@ -26,7 +27,13 @@ namespace WebAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1)
+                //Solved Naming Convention in Json Result
+                .AddJsonOptions(Options => {
+                    var resolver = Options.SerializerSettings.ContractResolver;
+                    if (resolver != null)
+                        (resolver as DefaultContractResolver).NamingStrategy = null;
+                });
             //add DB service
             services.AddDbContext<TaskDetailsContext>(options =>
             options.UseSqlServer(Configuration.GetConnectionString("DevConnection")));
