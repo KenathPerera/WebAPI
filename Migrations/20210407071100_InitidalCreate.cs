482d682bedd0d6b78f64fb530f8cb851f88f9bc2
kenath\kenath perera <kenathperera222@gmail.com>
2021-04-07 17:13:30 +1000

    Create Database Using Entity Framework
    Add-Migration
    Update-Database

---
+++ Migrations/20210407071100_InitidalCreate.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebAPI.Migrations
+{
+    public partial class InitidalCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TaskDetails",
+                columns: table => new
+                {
+                    TaskId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Progress = table.Column<int>(nullable: false),
+                    DueDate = table.Column<string>(type: "varchar(10)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TaskDetails", x => x.TaskId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TaskDetails");
+        }
+    }
+}
