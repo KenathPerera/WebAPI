b6b8f4c3462283ff1dd9bb89814862a02c3fdce8
kenath\kenath perera <kenathperera222@gmail.com>
2021-04-07 17:08:46 +1000

    Initial commit

---
+++ Models/TaskDetail.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Models
+{
+    public class TaskDetail
+    {
+        [Key]
+        public int TaskId { get; set; }
+        [Required]
+        public int Progress { get; set; }
+        [Required]
+
+        [Column(TypeName = "varchar(10)")]
+        public string DueDate { get; set; }
+    }
+}
---
+++ Models/TaskDetailsContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Models
+{
+    public class TaskDetailsContext : DbContext
+    {
+        //information Db provide(mqsql/SQL),Connection String for DB
+        public TaskDetailsContext(DbContextOptions<TaskDetailsContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<TaskDetail> TaskDetails { get; set; }
+    }
+}
